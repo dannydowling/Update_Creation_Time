592b5b30bbd33ed8edf91c893edef081d7f4422f
Danny Dowling <danny.dowling@gmail.com>
2023-11-07 09:07:10 -0900

    update

--- Program.cs
+++ Program.cs
@@ -10,34 +10,36 @@ public class UpdateFileCreationTime
         if (args.Length == 0)
         {
             directoryPath = Directory.GetCurrentDirectory();
-            directoryPath = Directory.GetParent(directoryPath).FullName;
         }
         else
         {
-           directoryPath = args[0];
+            directoryPath = args[0];
         }
         UpdateCreationTime(directoryPath);
         Console.WriteLine("File creation times updated in directory: " + directoryPath);
     }
-
-    static void UpdateCreationTime(string directoryPath)
+    private static void UpdateCreationTime(string directoryPath)
     {
-        string[] files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
-
-        foreach (string file in files)
+        try
         {
-            try
+            foreach (string d in Directory.GetDirectories(directoryPath))
+            {
+                UpdateCreationTime(d);
+            }
+            foreach (var file in Directory.GetFiles(directoryPath))
             {
                 FileInfo fileInfo = new FileInfo(file);
                 fileInfo.CreationTime = DateTime.Now;
-                Console.WriteLine("Updated creation time for file: " + fileInfo.FullName);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error updating creation time for file: " + file);
-                Console.WriteLine(ex.Message);
+                fileInfo.LastWriteTime = DateTime.Now;
+                fileInfo.LastAccessTime = DateTime.Now;
+                Console.WriteLine("Updated time for file: " + fileInfo.FullName);
             }
         }
+        catch (System.Exception e)
+        {
+            Console.WriteLine("eeroooorrrr");
+        }
+    
     }
 }
 
