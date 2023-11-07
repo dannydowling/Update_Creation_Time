e44e50cd901f0e4334136376b1364cb71631a7a6
danny <danny@AETHELRED>
2023-11-07 07:18:44 -0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+public class UpdateFileCreationTime
+{
+    public static void Main(string[] args)
+    {
+        string directoryPath;
+        if (args.Length == 0)
+        {
+            directoryPath = Directory.GetCurrentDirectory();
+            directoryPath = Directory.GetParent(directoryPath).FullName;
+        }
+        else
+        {
+           directoryPath = args[0];
+        }
+        UpdateCreationTime(directoryPath);
+        Console.WriteLine("File creation times updated in directory: " + directoryPath);
+    }
+
+    static void UpdateCreationTime(string directoryPath)
+    {
+        string[] files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+
+        foreach (string file in files)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                fileInfo.CreationTime = DateTime.Now;
+                Console.WriteLine("Updated creation time for file: " + fileInfo.FullName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error updating creation time for file: " + file);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
