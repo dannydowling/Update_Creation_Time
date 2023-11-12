a11dec13b9b928f970ee021fa1ce36b9cf5aa4ac
Danny Dowling <danny.dowling@gmail.com>
2023-11-12 03:30:52 -0900

    update

--- Program.cs
+++ Program.cs
@@ -1,33 +1,53 @@
-﻿public class UpdateFileCreationTime
+﻿using System.Globalization;
+
+public class UpdateFileCreationTime
 {
     public static void Main(string[] args)
     {
-        string directoryPath;
-        if (args.Length == 0)
-        {
-            directoryPath = Directory.GetCurrentDirectory();
-        }
+        string directoryPath = Directory.GetCurrentDirectory();
+        double creationTime;
+
+        if (args.Length != 0)
+        {  creationTime = Convert.ToDouble(args[0]);  }
         else
-        {
-            directoryPath = args[0];
-        }
-        UpdateCreationTime(directoryPath);
-        Console.WriteLine("File creation times updated in directory: " + directoryPath);
+        {  creationTime = Convert.ToDouble(DateTime.Now);  }
+
+        UpdateCreationTime(directoryPath, creationTime);
+        
     }
-    private static void UpdateCreationTime(string directoryPath)
+  
+    public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+    {
+        // Unix timestamp is seconds past epoch
+        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+        return dateTime;
+    }
+
+    private static void UpdateCreationTime(string directoryPath, double creationTime)
     {
         try
         {
             foreach (string d in Directory.GetDirectories(directoryPath))
             {
-                UpdateCreationTime(d);
+                UpdateCreationTime(d, creationTime);
+
+                // I suspect this is doing a lot of things over and over...
+
+                DirectoryInfo directoryInfo = new DirectoryInfo(d);
+                directoryInfo.CreationTime = UnixTimeStampToDateTime(creationTime);
+                Console.WriteLine("File creation times updated in directory: " + directoryPath);
+
             }
             foreach (var file in Directory.GetFiles(directoryPath))
             {
+                double timeBase = Convert.ToDouble(file.GetHashCode());
+                var randomBase = new Random();
+
                 FileInfo fileInfo = new FileInfo(file);
-                fileInfo.CreationTime = DateTime.Now;
-                fileInfo.LastWriteTime = DateTime.Now;
-                fileInfo.LastAccessTime = DateTime.Now;
+                fileInfo.CreationTime = UnixTimeStampToDateTime(creationTime);             
+                fileInfo.LastWriteTime = UnixTimeStampToDateTime(creationTime = randomBase.Next(1, 166365));
+                fileInfo.LastAccessTime = UnixTimeStampToDateTime(creationTime + randomBase.Next(166365, 322685));
                 Console.WriteLine("Updated time for file: " + fileInfo.FullName);
             }
         }
