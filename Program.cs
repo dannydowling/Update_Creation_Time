2e2d95c0345b1bcb55dfe9d8ca30d5d9e48da0fe
Danny Dowling <danny.dowling@gmail.com>
2023-11-26 14:12:20 -0900

    added a compiled version into a folder for people that don't want to compile it.

--- Program.cs
+++ Program.cs
@@ -48,28 +48,27 @@
         }
     }
 
-    public static DateTime UnixTimeStampToDateTime(double creationTime)
+    public static DateTime UnixTimeStampToDateTime(double time)
     {
         // Unix timestamp is seconds past epoch
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(creationTime).ToLocalTime();
+        dateTime = dateTime.AddSeconds(time).ToLocalTime();
         return dateTime;
     }
 
-    private static void UpdateCreationTime(string directoryPath, Int32 arg)
+    private static void UpdateCreationTime(string directoryPath, Int32 offset)
     {
         try
         {
             foreach (string d in Directory.GetDirectories(directoryPath))
             {
-
                 DirectoryInfo directoryInfo = new DirectoryInfo(d);
-                directoryInfo.CreationTime = UnixTimeStampToDateTime(777777777 + arg);
-                directoryInfo.LastWriteTime = UnixTimeStampToDateTime(888888888 + arg);
-                directoryInfo.LastAccessTime = UnixTimeStampToDateTime(999999999 + arg);
+                directoryInfo.CreationTime = UnixTimeStampToDateTime(777777777 + offset);
+                directoryInfo.LastWriteTime = UnixTimeStampToDateTime(888888888 + offset);
+                directoryInfo.LastAccessTime = UnixTimeStampToDateTime(999999999 + offset);
                 Console.WriteLine("Updated Directory Info for Directory: " + directoryInfo.FullName);
 
-                UpdateCreationTime(d, arg);
+                UpdateCreationTime(d, offset);
 
             }
             foreach (var file in Directory.GetFiles(directoryPath))
@@ -78,9 +77,9 @@
 
                 FileInfo fileInfo = new FileInfo(file);
                 fileInfo.IsReadOnly = false;
-                fileInfo.CreationTime = UnixTimeStampToDateTime(999999999 + arg);
-                fileInfo.LastWriteTime = UnixTimeStampToDateTime(999999999 + arg);
-                fileInfo.LastAccessTime = UnixTimeStampToDateTime(999999999 + arg);
+                fileInfo.CreationTime = UnixTimeStampToDateTime(999999999 + offset);
+                fileInfo.LastWriteTime = UnixTimeStampToDateTime(999999999 + offset);
+                fileInfo.LastAccessTime = UnixTimeStampToDateTime(999999999 + offset);
                 Console.WriteLine("Updated time for file: " + fileInfo.FullName);
             }
         }
